ede0f6f9d19472860656c069bb21733da495144c
Nathan Boehringer <nathan@linebreak.dev>
2020-01-06 19:09:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var x = add(1, 2);
     y = subtract(1, 2);
     z = multiply(1, 2);
+    d = divide(1, 2);
+    
 
 function add(a, b) {
   return a + b;            
@@ -11,4 +13,6 @@ function subtract(a, b) {
 function multiply(a, b) {
   return a * b;            
 }
-x;
+function divide(a, b) {
+  return a / b;            
+}
