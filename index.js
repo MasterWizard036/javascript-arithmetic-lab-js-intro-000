b0328a29231ad03e78afac363c1c6dc594955a4b
Nathan Boehringer <nathan@linebreak.dev>
2020-01-06 18:55:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-Function; add(addition);{
-return 1 + 2;
+var x = myFunction(1, 2);   // Function is called, return value will end up in x
+
+function myFunction(a, b) {
+  return a * b;             // Function returns the product of a and b
 }
-return addition;
+x;
