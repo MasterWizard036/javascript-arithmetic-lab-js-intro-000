71e10c1fa773a8c84b4c34e4774c55b770e52806
Nathan Boehringer <nathan@linebreak.dev>
2020-01-06 19:05:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-var x = myFunction(1, 2);  
+var x = add(1, 2);
+    y = subtract(1, 2);
 
 function add(a, b) {
+  return a + b;            
+}
+
+
+function subtract(a, b) {
   return a + b;            
 }
 x;
