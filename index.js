ad47992431747128be1072536252cdc0de5700c8
Nathan Boehringer <nathan@linebreak.dev>
2020-01-06 18:55:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var x = myFunction(1, 2);   // Function is called, return value will end up in x
 
 function myFunction(a, b) {
-  return a * b;             // Function returns the product of a and b
+  return a + b;             // Function returns the product of a and b
 }
 x;
